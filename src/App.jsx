e597d7506c3a95db9edd96479bd9f47cd44ba979
Nacho <nachoballestrino02@gmail.com>
2024-03-07 20:28:51 -0300

    Slider handle left and right arrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import InitialPage from './components/InitialPage';
 import AboutMe from './components/AboutMe';
+import Technologies from './components/Technologies';
 
 function App() {
   return (
     <>
       <InitialPage />
       <AboutMe />
+      <Technologies />
     </>
   );
 }
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,5 +1,60 @@
 function Technologies() {
-  return <div>Technologies</div>;
+  //css, html, git, github, javascript, react, python, SQL, tailwind, vite
+  let elements = [
+    'css.png',
+    'html.png',
+    'git.png',
+    'github.png',
+    'javascript.png',
+    'react.png',
+    'python.png',
+    'SQL.png',
+    'tailwind.png',
+    'vite.png',
+  ];
+
+  const handleRightArrow = () => {
+    let newElements = [];
+    for (let i = 0; i < elements.length; i++) {
+      //example with i = 0
+      let previousElement = elements[i - 1]; //vite
+      if (previousElement === undefined) {
+        previousElement = elements[elements.length - 1];
+        newElements[i] = previousElement; //vite
+        continue;
+      }
+      //asignamos a previous element el ultimo valor de el array elements
+      newElements[i] = previousElement; //vite
+    }
+    elements = newElements;
+    console.log(elements);
+  };
+
+  const handleLeftArrow = () => {
+    let newElements = [];
+    for (let i = 0; i < elements.length; i++) {
+      //example with i = 0
+      let nextElement = elements[i + 1]; //vite
+      if (nextElement === undefined) {
+        nextElement = elements[0];
+        newElements[i] = nextElement; //vite
+        continue;
+      }
+      //asignamos a previous element el ultimo valor de el array elements
+      newElements[i] = nextElement; //vite
+    }
+    elements = newElements;
+    console.log(elements);
+  };
+
+  //test funcionamiento
+  return (
+    <section className='h-screen w-screen bg-gray-100'>
+      <button onClick={handleLeftArrow}>left</button>
+      <hr />
+      <button onClick={handleRightArrow}>right arrow</button>
+    </section>
+  );
 }
 
 export default Technologies;
